b0ee86111ee2692a1e980bb6eec38517dff6e667
Catalan-Nick <catalan1222@yahoo.com>
2022-09-19 15:55:30 -0500

    Added Day button

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ function formatClock(){
     return format;
 }
 function sunlight(){
-
+    
+if (document.getElementById("sun-checkbox").checked){
+    document.getElementById("d-label").innerText = "Switch to Night Mode";
+    document.getElementById('clock').style.backgroundImage = "url('https://images.unsplash.com/photo-1630148180214-417337ce9652?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80')";
+    // document.getElementById('clock').style.color = "blue";
+}
+else{
+    document.getElementById("d-label").innerText = "Switch to Day Mode";
+    document.getElementById('clock').style.backgroundImage = "url('https://images.unsplash.com/photo-1481819613568-3701cbc70156?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80')"
+}
 }
 function currentTime(){
     let date = new Date();
@@ -18,11 +27,18 @@ function currentTime(){
 
     let format = formatClock();
     let meridiem = "";
+    // if format is in 12hr clock position
     if (format === 12){
-        meridiem = " AM";
+        //meridiem has to be checked before hours are converted
+        if (hours >= 12){
+            meridiem = " PM";
+        }
+        else if (hours < 12 || hours == 24){
+            meridiem = " AM"
+        }
+
         if (hours > 12){
             hours = hours - 12;
-            meridiem = " PM"
         }
     }
     hours = (hours < 10) ? "0" + hours : hours;
